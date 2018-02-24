0d10f1867d35b5bac5f234e06e761fa7db63a509
Viktoriia Nosova <viktoriianosova@hotmail.com>
2018-02-24 22:01:49 +0800

    add eslint / clean up the project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Welcome from './Welcome';
-import Clock from './Clock';
-import List from './List';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <hr />
-        <Welcome>
-        Vika
-        </Welcome>
-        <hr />
-        <Clock />
-        <hr />
-        <List />
+      <div>
+        <h1>Сhat</h1>
       </div>
     );
   }
